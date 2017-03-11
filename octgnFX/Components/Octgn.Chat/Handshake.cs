e9ce714df5ad34e839a7c654d9bb7771b60f1590
Kelly Elton <its.the.doc@gmail.com>
2017-03-11 01:12:13 -0500

    Starting handshake...need handshake so the chat service can figure out who you are based on your address

---
+++ octgnFX/Components/Octgn.Chat/Handshake.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace Octgn.Chat
+{
+    public class Handshake
+    {
+        public string Username { get; set; }
+        public string Password {
+            get {
+                IntPtr unmanagedString = IntPtr.Zero;
+                try {
+                    unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(_password);
+                    return Marshal.PtrToStringUni(unmanagedString);
+                } finally {
+                    Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+                }
+            }
+            set {
+                _password.Clear();
+                foreach (char c in value?.ToCharArray()) {
+                    _password.AppendChar(c);
+                }
+            }
+        }
+
+        private readonly SecureString _password = new SecureString();
+    }
+
+    public class HandshakeResponse
+    {
+        public string SessionId { get; set; }
+    }
+}
